5c4fdd716beca84700201cc35de8ad7a6e937e9c
Eduardo Morales <emoral435@gmail.com>
2024-05-10 19:02:17 -0500

    feat: added dark and light mode
    Signed-off-by: Eduardo Morales <emoral435@gmail.com>

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,26 @@
-<script>
-	import Welcome from "./Welcome.svelte";
-</script>
-
 <div>Hello.</div>
-<Welcome />
 <Welcome project="Not Swole Goal" />
+<Button 
+	on:click={() => alert('It worked!')}
+	color="secondary">
+	Click me!
+</Button>
+<Button 
+	on:click={() => alert('It worked!')}
+	color="primary">
+	Click me too!
+</Button>
+<IconButton on:click={() => clicked++}>
+	<Icon tag="svg" viewBox="0 0 24 24">
+		<path fill="currentColor" d={mdiFormatColorFill} />
+	</Icon>
+</IconButton>
+
+<script lang="ts">
+	import Welcome from "./Welcome.svelte"
+	import Button from "@smui/button"
+	import IconButton, { Icon } from '@smui/icon-button'
+	import { mdiFormatColorFill } from '@mdi/js';
+
+	let clicked = 0
+</script>
